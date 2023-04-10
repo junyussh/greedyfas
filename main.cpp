0b64702bf64e999f1e642eca34b48c0cedebc1aa
Chun Yu <junyussh@gmail.com>
2023-04-10 12:25:08 +0800

    chore: remove unused comments and variables, add some useful comments

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-struct Node
-{
-    int value;
-    Node *next;
-};
-map<int, Node *> vertex;
-// sink affect other nodes' out-degree
+// Removing the sink affects the out-degree of other nodes
 void removeSink(vector<vector<int>> &graph, vector<bool> &eliminated, vector<int> &outdegree, int u)
 {
+    // Find which node is connected to u
     for (int i = 0; i < (int)graph.size(); i++)
     {
         if (!eliminated[i])
@@ -24,10 +20,10 @@ void removeSink(vector<vector<int>> &graph, vector<bool> &eliminated, vector<int
         }
     }
 }
-// source affect other nodes' in-degree
+
+// Removing the source will affect the in-degree of other nodes
 void removeSource(vector<vector<int>> &graph, vector<bool> &eliminated, vector<int> &indegree, int u)
 {
-    // cout << u << endl;
     for (int v : graph[u])
     {
         if (!eliminated[v])
@@ -36,14 +32,21 @@ void removeSource(vector<vector<int>> &graph, vector<bool> &eliminated, vector<i
         }
     }
 }
+
 vector<int> greedyfas(vector<vector<int>> &graph)
 {
     const int n = graph.size();
+
     vector<int> s1, s2;
     vector<int> indegree(n, 0);
     vector<int> outdegree(n, 0);
+
+    // state of nodes
     vector<bool> eliminated(graph.size(), false);
+    // Number of remaining nodes
     int rest = n;
+
+    // compute the in-degree and out-degree of each node
     for (int i = 0; i < n; i++)
     {
         for (int j : graph[i])
@@ -53,6 +56,7 @@ vector<int> greedyfas(vector<vector<int>> &graph)
         }
     }
 
+    // if the graph is not empty
     while (rest != 0)
     {
         // sink: out-degree == 0
@@ -68,15 +72,11 @@ vector<int> greedyfas(vector<vector<int>> &graph)
                 removeSink(graph, eliminated, outdegree, u);
             }
         }
+        // when all nodes are eliminated, exit loop
         if (rest == 0)
         {
             break;
         }
-
-        // if (!has_sink)
-        // {
-        //     break;
-        // }
 
         // source: in-degree == 0
         bool has_source = false;
@@ -91,19 +91,19 @@ vector<int> greedyfas(vector<vector<int>> &graph)
                 removeSource(graph, eliminated, indegree, u);
             }
         }
-        // if (!has_source)
-        // {
-        //     break;
-        // }
+        // when all nodes are eliminated, exit loop
         if (rest == 0)
         {
             break;
         }
+
+        // find the node with maximum δ(u)
         int u_max = -1, delta_max = -1;
         for (int u = 0; u < n; u++)
         {
             if (!eliminated[u])
             {
+                // δ(u) = outdegree(u) - indegree(u)
                 int delta = outdegree[u] - indegree[u];
                 if (delta > delta_max)
                 {
@@ -112,71 +112,22 @@ vector<int> greedyfas(vector<vector<int>> &graph)
                 }
             }
         }
+
         s1.push_back(u_max);
         removeSink(graph, eliminated, outdegree, u_max);
         removeSource(graph, eliminated, indegree, u_max);
         eliminated[u_max] = true;
         rest--;
-
-        // int best_sink = -1;
-        // int best_source = -1;
-        // for (int i = 0; i < n; i++)
-        // {
-        //     if (!eliminated[i])
-        //     {
-        //         // find sink node
-        //         if (outdegree[i] == 0)
-        //         {
-        //             s1.push_back(i);
-        //             best_sink = i;
-        //             eliminated[i] = true;
-        //             break;
-        //         }
-        //     }
-        // }
-        // for (int i = 0; i < n; i++)
-        // {
-        //     if (!eliminated[i])
-        //     {
-        //         // source node
-        //         if (indegree[i] == 0)
-        //         {
-        //             best_source = i;
-        //             s2.push_back(i);
-        //             eliminated[i] = true;
-        //             break;
-        //         }
-        //     }
-        // }
-        // // update in-degree
-        // for (int i = 0; i < graph[best_sink].size(); ++i)
-        // {
-        //     int neighbor = graph[best_sink][i];
-        //     if (!eliminated[neighbor])
-        //     {
-        //         indegree[neighbor]--;
-        //     }
-        // }
     }
+
     vector<int> s;
     s.insert(s.end(), s1.begin(), s1.end());
     s.insert(s.end(), s2.rbegin(), s2.rend());
+
     return s;
 }
 int main(int argc, char const *argv[])
 {
-    // int n;
-    // scanf("%d", &n);
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int v1, v2;
-    //     scanf("%d %d", &v1, &v2);
-    //     if (vertex.count(v1) == 0)
-    //     {
-    //         vertex.insert({v1, new Node{v1, NULL}});
-    //     }
-
-    // }
     vector<vector<int>> graph = {
         {1},
         {0, 2},
@@ -193,11 +144,14 @@ int main(int argc, char const *argv[])
         {0},
         {1, 2}
     };
+
     vector<int> fas = greedyfas(graph);
+
     for (int u : fas)
     {
         cout << u << " -> ";
     }
+    
     cout << endl;
 
     return 0;
